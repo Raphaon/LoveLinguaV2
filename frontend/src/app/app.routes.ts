ebb9fdbb9deff4979ce182dc13ebd67590bfd6db
Rapha <raphaondobo@gmail.com>
2025-12-05 21:56:53 -0500

    Creation d'un projet frontend ionic bien structure

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,19 @@
+import { Routes } from '@angular/router';
+import { HomePage } from './home/home.page';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./pages/login/login.page').then((m) => m.LoginPage),
+  },
+  {
+    path: 'home',
+    component: HomePage,
+  },
+];
--- frontend/src/app/pages/login/login.page.ts
+++ frontend/src/app/pages/login/login.page.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule, // <--- Tous les composants Ionic viennent de là
+  ],
 })
 export class LoginPage {
   credentials = {
